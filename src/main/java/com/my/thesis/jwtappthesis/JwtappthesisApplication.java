69ed64498595e4a2097c4632c538264c892a1e72
Denys Davydov <davydov654@gmail.com>
2022-04-23 21:12:11 +0300

    Initial commit

---
+++ src/main/java/com/my/thesis/jwtappthesis/JwtappthesisApplication.java
@@ -0,0 +1,13 @@
+package com.my.thesis.jwtappthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtappthesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtappthesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/thesis/jwtappthesis/JwtappthesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.thesis.jwtappthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtappthesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
